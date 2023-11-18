c8fb23ad444f98216eaa0c6ce5b8eccba76f6127
Shalitha <shalithamorapitiya@gmail.com>
2023-11-19 01:27:58 +0530

    add - game Layout

--- roller_dice_app/lib/roller_dice.dart
+++ roller_dice_app/lib/roller_dice.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+const numberOfRounds = 5;
+
 class RollerDice extends StatefulWidget {
   const RollerDice({super.key});
 
@@ -10,35 +12,109 @@ class RollerDice extends StatefulWidget {
 }
 
 class _RollerDiceState extends State<RollerDice> {
- 
-  var diceNumer = 1;
+  int diceNumber = 1;
+  int round = 0;
+  int playerOneWins = 0;
+  int playerTwoWins = 0;
+  int playerOneRound = 1;
+  int playerTwoRound = 1;
+  List<int>playerOneScores=[];
+  List<int>playerTwoScores=[];
 
   void roll() {
     setState(() {
-      diceNumer = Random().nextInt(6) +1;
+      diceNumber = Random().nextInt(6) + 1;
     });
-    
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisSize: MainAxisSize.min, children: [
+    return Column(mainAxisSize: MainAxisSize.min, 
+    children: [
+      Text(
+        'Round ${(round)+1}',
+        style: const TextStyle(color: Colors.white,fontSize: 28),
+      ),
+      const SizedBox(
+        height: 20,
+      ),
       Image.asset(
-        'assets/images/dice-$diceNumer.png',
+        'assets/images/dice-$diceNumber.png',
         width: 200,
       ),
       const SizedBox(
         height: 20,
       ),
-      TextButton(
+      Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextButton(
           onPressed: roll,
           style: TextButton.styleFrom(
             backgroundColor: Colors.amber,
             foregroundColor: Colors.black,
             textStyle: const TextStyle(fontSize: 28),
           ),
-          child: const Text('Roller once'))
+          child: const Text('Player 1')),
+          const SizedBox(
+            width: 20,
+          ),
+          TextButton(
+          onPressed: roll,
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.amber,
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Player 2'))
+        ],
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      Text(
+        'Player 1 : $playerOneWins - $playerTwoWins : Player 2',
+        style: const TextStyle(color: Colors.white,fontSize: 24),
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              const Text(
+                'Round#',style: TextStyle(color :Colors.black, fontSize: 20)),
+                for (int r=1;r<=playerOneScores.length;r++)
+                  Text(
+                    'Round $r'
+                  )
+            ],
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Column(
+            children: [
+              const Text('Player 1',
+              style: TextStyle(color: Colors.black,fontSize: 20)),
+              for(var value in playerOneScores)Text(value.toString())
+            ],
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Column(
+            children: [
+              const Text('Player 2',
+              style: TextStyle(color: Colors.black,fontSize: 20)),
+              for(var value in playerTwoScores)Text(value.toString())
+            ],
+          ),
+        ],
+      
+      ),
     ]);
   }
 }
-
